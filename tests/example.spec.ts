9cc11ddde55976d2ade68146a7fa3a6ef9b12ad8
KJR020 <johnjiro1114@gmail.com>
2025-04-12 06:52:31 +0900

    ✅ Update app title and add title check in home page test

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -2,5 +2,9 @@ import { test, expect } from "@playwright/test";
 
 test("should navigate to home page", async ({ page }) => {
   await page.goto("/");
+
+  // Check if the home page is loaded
   await expect(page).toHaveURL("/");
+
+  await expect(page).toHaveTitle("Document App");
 });
